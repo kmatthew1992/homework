f74aabb070b449b0986e486d6ffa1c465c6f5db5
kmatthew1992 <koszegim8@gmail.com>
2024-03-03 12:01:10 +0100

    even numbers method

---
+++ Even.java
@@ -0,0 +1,33 @@
+package homework;
+
+import java.util.Scanner;
+
+public class Even {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num1 = numbers(scanner, "Adjon meg az egyik egész számot a -100 és 100 között: ");
+        int num2 = numbers(scanner, "Adjon meg a másik egész számot az -100 és 100 között: ");
+
+        System.out.println("A páros számok a két megadott szám között:");
+
+        for (int i = Math.min(num1, num2); i <= Math.max(num1, num2); i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+    public static int numbers(Scanner scanner, String prompt) {
+        int num;
+        do {
+            System.out.print(prompt);
+            while (!scanner.hasNextInt()) {
+                System.out.println("Kérem,hogy egész számot adjon meg!");
+                System.out.print(prompt);
+                scanner.next();
+            }
+            num = scanner.nextInt();
+        } while (num < -100 || num > 100);
+        return num;
+    }
+}
